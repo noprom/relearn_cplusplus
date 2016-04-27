2545393f5c3c985ea0a184fbc0a6f4b8d7d9164f
noprom <tyee.noprom@qq.com>
2016-04-27 13:51:50 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  relearn_c++_primer
+//
+//  Created by noprom on 4/27/16.
+//  Copyright © 2016 tyee.noprom@qq.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
